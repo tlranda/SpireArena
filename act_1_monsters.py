2cdca3b42b8ece308617e62230ffbbe72c5f81b9
FuronInvader <77162016+FuronInvader@users.noreply.github.com>
2021-04-14 22:19:15 -0400

    Update act_1_monsters.py
    Adds Louse, Jaw Worm, Slaver. All of which need work.
    Working on powers
    Special intent not defined for any monster added

--- act_1_monsters.py
+++ act_1_monsters.py
@@ -137,3 +137,434 @@ class AcidSlime(arena.Monster):
 		#dealt, targets = self.Damage(*damages)
 		print(f"{str(self)} uses TACKLE to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage to {[str(monster) for monster in targets]}!!")
 
+
+
+
+
+
+
+
+# work in progress
+
+class Slaver(arena.Monster):
+
+	'''
+	Known Parties:
+		Act 1: 
+			Blue Slaver
+			Blue Slaver, Louse 
+			Blue Slaver, Acid Slime (M)
+			Blue Slaver, Spike Slime (M)
+			Red Slaver
+			Red Slaver, Louse 
+			Red Slaver, Acid Slime (M)
+			Red Slaver, Spike Slime (M)
+		Act 2:
+			Blue Slaver, Red Slaver, TaskMaster
+	'''
+
+	def __init__(self, ID="<TemplateSlaver>", Arena=None, Friendlies=None, ascension=0, variant=0):
+		# Ensure proper type
+		ascension, variant = int(ascension), int(variant)
+		# Constructor
+		super().__init__(ID, Arena, Friendlies)
+		self.Color = ['Blue', 'Red'][variant]
+		self.Name = f"{self.Color} Slaver"
+		self.Act = 1
+		# Stats
+		if self.Color == 'Blue':
+			self.MaxHealth = self.rng.randint(46,50)
+			self.AscendMaxHealth = 2
+		else:
+			self.MaxHealth = self.rng.randint(46,50)
+			self.AscendMaxHealth = 2
+		# Moves
+		self.makeMoves((powers.SOURCE.ATTACK, self.Stab, f"{str(self)} stabs"),
+						(powers.SOURCE.ATTACK, self.Rake, f"{str(self)} rakes"),
+						(powers.SOURCE.ATTACK, self.Scrape, f"{str(self)} scrapes"),
+						(powers.SOURCE.SKILL, self.Entangle, f"(str(self)} throws net")
+		if self.Color == 'Blue':
+			self.Pattern = [0.4, 0.6, 0, 0]
+		else:
+			#self.Pattern = [1, 0, 0, 0] first turn 
+			self.Pattern = [0.3375, 0, 0.4125, 0.25] #pattern is scrape, scrape, stab until entangle (25% per tern)
+			#self.Pattern = [0.45, 0, 0.55, 0]  after entangle
+		# Powers
+		# self.PowerPool.Append(Power(Entangle)) # stops target from attacking for one turn]
+		# Ascend the mortal form
+		self.ascension = ascension
+		if ascension > 0:
+			self.Ascend(ascension)
+		# Call Reset to propagate everything properly
+		self.Reset()
+
+	def Ascend(self, ascension):
+		if self.ascension < 7 and ascension >= 7:
+			self.MaxHealth += self.AscendMaxHealth
+			self.Pattern = [0.4, 0.3, 0.3]
+		elif self.ascension >= 7 and self.ascension < 7:
+			self.MaxHealth -= self.AscendMaxHealth
+			self.Pattern = [0.3, 0.4, 0.3]
+'''
+	def SpecialIntent(self, moveCall, moveAlternatives, moveChances):
+
+'''
+	def Stab(self):
+		#Does damage to single target
+		if self.Color == 'Blue':
+			if self.ascension >= 2:
+				damage = 13
+			else:
+				damage = 12
+		elif self.Color == 'Red':
+			if self.ascension >= 2:
+				damage = 13
+			else:
+				damage = 14
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		print(f"{str(self)} uses Stab to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage to you!!")
+
+	def Rake(self):
+		#just for Blue Slaver applys weak and does damage to single target
+		if self.ascension >= 2:
+			damage = 8
+		else: 
+			damage = 7
+		
+		if self.ascension >= 17:
+			weak = 2
+		else:
+			weak = 1		
+		
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		# Apply weak to target
+		weaknessPower = powers.Power(timings=powers.TRIGGER.OFFENSE, priority=1, turns=weak, callback=powers.WEAK, AffectDescription=powers.DESCRIPTIONS.WEAK)
+		targets = self.ApplyPowers(weaknessPower, affectClass=self.Abilities[self.Callbacks.index(self.Rake)].affectClass,
+						ArenaTargets=1, ArenaSelf=False, ArenaAll=False,
+						GroupTargets=1, GroupOnlySelf=False, GroupIncludeSelf=False, GroupAll=True, GroupCheckAlive=True,
+						extras=[])
+		print(f"{str(self)} uses Rake to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage and apply {weak} weak to {[str(monster) for monster in targets]}!!")
+
+	def Scrape(self):
+		if self.Color == 'Red':
+			if self.ascension >= 2:
+				damage = 9
+			else:
+				damage = 8
+			if self.ascension >= 17:
+				vulnerable = 2
+			else:
+				vulnerable = 1
+		else: # self.Color = 'Blue'
+			assert False, "Error: Blue Slaver cannot use Scrape!"
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		# How you would do a multihit:
+		#damages = [damage, damage]
+		#dealt, targets = self.Damage(*damages)
+		#Applies Vulnerable
+		vulnerablePower = powers.Power(timings=powers.TRIGGER.DEFENSE, priority=3, turns=vulnerable, callback=powers.VULNERABLE, AffectDescription=powers.DESCRIPTIONS.VULNERABLE)
+		targets = self.ApplyPowers(vulnerablePower, affectClass=self.Abilities[self.Callbacks.index(self.Scrape)].affectClass,
+						ArenaTargets=1, ArenaSelf=False, ArenaAll=False,
+						GroupTargets=1, GroupOnlySelf=False, GroupIncludeSelf=False, GroupAll=True, GroupCheckAlive=True,
+						extras=[])
+		print(f"{str(self)} uses Scrape to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage and apply {vulnerable} vulnerable to {[str(monster) for monster in targets]}!!")
+	
+	def Entangle(self):
+		#Entangle stops target from using attack in
+		if self.Color == 'Red':
+			entangle = 1
+		else: # self.Color == 'Blue':
+			assert False, "Error: Blue Slaver cannot use Entangle!"
+		print(f"{str(self)} uses Entangle to apply 1 entangle!")
+
+'''
+work in progress
+needs strength power
+needs act based varients
+'''
+class JawWorm(arena.Monster):
+	
+	''Known Parties:
+		Act 1:
+			Jaw Worm
+			Jaw Worm, Louse
+			Jaw Worm, Acid Slime (M)
+			Jaw Worm, Spike Slime (M)
+		Act 3:
+			Jaw Worm *3
+	'''
+
+	def __init__(self, ID="<TemplateJawWorm>", Arena=None, Friendlies=None, ascension=0, variant=0):
+		# Ensure proper type
+		ascension, variant = int(ascension), int(variant)
+		# Constructor
+		super().__init__(ID, Arena, Friendlies)
+		self.Name = f"Jaw Worm"
+->important		self.variant? = 2
+		self.Act = 1
+		# Stats
+		self.MaxHealth = self.rng.randint(40,44)
+		self.AscendMaxHealth = 2
+
+		# Moves
+		self.makeMoves((powers.SOURCE.ATTACK, self.Chomp, f"{str(self)} chomps"),
+						(powers.SOURCE.ATTACK, self.Thrash, f"{str(self)} thrashs"),
+						(powers.SOURCE.POWER, self.Bellow, f"{str(self)} bellows"))
+
+		self.Pattern = [0.25, 0.30, 0.45]
+
+		# Powers
+		# self.PowerPool = [Power(SPLIT) # at/below half HP splits into 2 Acid Slime (M) with current HP]
+		# Ascend the mortal form
+		self.ascension = ascension
+		if ascension > 0:
+			self.Ascend(ascension)
+		# Call Reset to propagate everything properly
+		self.Reset()
+
+	def Ascend(self, ascension):
+		if self.ascension >= 7:
+			self.MaxHealth += self.AscendMaxHealth
+'''
+
+if act 1: 
+	starts combat with chomp
+	cannot use bellow 2 in a row 
+	cannot use thrash 3 time in a row
+	cannot use chomp 2 times in a row
+if act 3: 
+	starts combat with the bellow power cast
+	doesnt have to start with chomp
+'''
+'''
+	def SpecialIntent(self, moveCall, moveAlternatives, moveChances):
+
+'''
+
+	def Chomp(self):
+		if self.ascension >= 2:
+			damage = 12
+		else:
+			damage = 11
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		print(f"{str(self)} uses Chomp to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage to you!")
+
+	def Thrash(self):
+		damage = 7
+		block = 5
+		# Deal damage and gains block
+		dealt, targets = self.Damage(damage)
+		print(f"{str(self)} uses Thrash to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage to you and gains 5 block!")
+
+	def Bellow(self):
+		block = 6
+		if self.ascension >= 2 and self.ascension < 17:
+			strength = 4
+		elif self.ascension >= 17:
+			strength = 5
+			block = 9
+		else:
+			strength = 3
+		# Gains strength and block
+		print(f"{str(self)} uses Bellow to gain {strength} and {block} block!")
+
+
+'''
+Work in progress
+Needs power cultist and ritual thing
+'''
+
+class Cultist(arena.Monster):
+
+	'''
+	Known Parties:
+		Act 1: 
+			Cultist
+			Cultist, Louse 
+			Cultist, Acid Slime (M)
+			Cultist, Spike Slime (M)
+		Act 2:
+			Cultist *3
+			Cultist, Chosen
+		Act 3:
+			Cultist *2, Awakened One
+	'''
+
+	def __init__(self, ID="<TemplateSlaver>", Arena=None, Friendlies=None, ascension=0, variant=0):
+		# Ensure proper type
+		ascension, variant = int(ascension), 1
+		# Constructor
+		super().__init__(ID, Arena, Friendlies)
+		self.Name = f"Cultist"
+		self.Act = 1
+		# Stats
+		self.MaxHealth = self.rng.randint(48, 54)
+		self.AscendMaxHealth = 2
+		# Moves
+		self.makeMoves((powers.SOURCE.ATTACK, self.Dark_Strike, f"{str(self)} stabs"),
+						(powers.SOURCE.POWER, self.Incantation, f"(str(self)} performs a ritual")
+		self.Pattern = [1, 0]
+
+		# Powers
+		# self.PowerPool.append(Power(Incantation)) # stops target from attacking for one turn]
+		# Ascend the mortal form
+		self.ascension = ascension
+		if ascension > 0:
+			self.Ascend(ascension)
+		# Call Reset to propagate everything properly
+		self.Reset()
+
+	def Ascend(self, ascension):
+		if self.ascension >= 7:
+			self.MaxHealth += self.AscendMaxHealth
+'''
+	def SpecialIntent(self, moveCall, moveAlternatives, moveChances):
+
+'''
+	def Dark_Strike(self):
+		#Does damage to single target
+		damage = 6
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		print(f"{str(self)} uses Stab to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage to you!!")
+
+	def Incantation(self):
+		#gains strength per tern
+		if self.ascension >= 4 and self.ascension < 17:
+			Ritual = 4
+		elif: 
+			Ritual = 5
+		else:
+			Ritual = 3
+		print(f"{str(self)} uses Incantation to gain Ritual {Ritual}!")
+
+
+
+
+'''
+work in progress
+needs power curl up
+"
+class Louse(arena.Monster):
+
+	'''
+	Known Parties:
+		Act 1: 
+			Louse *2 
+			Louse *3
+			Louse, Cultist
+			Louse, Looter
+			Louse, Red Slaver
+			Louse, Blue Slaver
+			Louse, Jaw Worm
+			Louse, Fungi Beast
+	'''
+
+	def __init__(self, ID="<TemplateSlaver>", Arena=None, Friendlies=None, ascension=0, variant=0):
+		# Ensure proper type
+		ascension, variant = int(ascension), int(variant)
+		# Constructor
+		super().__init__(ID, Arena, Friendlies)
+		self.Color = ['Red', 'Green'][variant]
+		self.Name = f"{self.Color} Louse"
+		self.Act = 1
+		# Stats
+		if self.Color == 'Red':
+			self.MaxHealth = self.rng.randint(10, 15)
+			self.AscendMaxHealth = 1
+		else:
+			self.MaxHealth = self.rng.randint(11, 17)
+			self.AscendMaxHealth = 1
+		# Moves
+		self.makeMoves((powers.SOURCE.ATTACK, self.Bite, f"{str(self)} bites"),
+						(powers.SOURCE.SKILL, self.Grow, f"{str(self)} gets larger"),
+						(powers.SOURCE.SKILL, self.Spit_Web, f"(str(self)} spits web")
+		if self.Color == 'Red':
+			self.Pattern = [0.75, 0.25, 0]
+		else:
+			self.Pattern = [0.75, 0, 0.25]
+
+		# Powers
+		# self.PowerPool = [Power(Curl Up) # stops target from attacking for one turn]
+		# Ascend the mortal form
+		self.ascension = ascension
+		if ascension > 0:
+			self.Ascend(ascension)
+		# Call Reset to propagate everything properly
+		self.Reset()
+
+	def Ascend(self, ascension):
+		if self.ascension < 7 and ascension >= 7:
+			self.MaxHealth += self.AscendMaxHealth
+			self.Pattern = [0.4, 0.3, 0.3]
+		elif self.ascension >= 7 and self.ascension < 7:
+			self.MaxHealth -= self.AscendMaxHealth
+			self.Pattern = [0.3, 0.4, 0.3]
+'''
+	def SpecialIntent(self, moveCall, moveAlternatives, moveChances):
+
+'''
+	def Stab(self):
+		#Does damage to single target
+		if self.Color == 'Blue':
+			if self.ascension >= 2:
+				damage = 13
+			else:
+				damage = 12
+		elif self.Color == 'Red':
+			if self.ascension >= 2:
+				damage = 13
+			else:
+				damage = 14
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		print(f"{str(self)} uses Stab to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage to you!!")
+
+	def Rake(self):
+		#just for Blue Slaver applys weak and does damage to single target
+		if self.ascension >= 2:
+			damage = 8
+		else: 
+			damage = 7
+		
+		if self.ascension >= 17:
+			weak = 2
+		else:
+			weak = 1	
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		# Apply weak to target
+		weaknessPower = powers.Power(timings=powers.TRIGGER.OFFENSE, priority=1, turns=weak, callback=powers.WEAK, AffectDescription=powers.DESCRIPTIONS.WEAK)
+		targets = self.ApplyPowers(weaknessPower, affectClass=self.Abilities[self.Callbacks.index(self.Rake)].affectClass,
+						ArenaTargets=1, ArenaSelf=False, ArenaAll=False,
+						GroupTargets=1, GroupOnlySelf=False, GroupIncludeSelf=False, GroupAll=True, GroupCheckAlive=True,
+						extras=[])
+		print(f"{str(self)} uses Rake to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage and apply {weak} weak to {[str(monster) for monster in targets]}!!")
+
+	def Scrape(self):
+
+		if self.ascension >= 2:
+			damage = 9
+		else:
+			damage = 8
+		if self.ascension >= 17:
+			vulnerable = 2
+		else:
+			vulnerable = 1
+
+		# Deal damage
+		dealt, targets = self.Damage(damage)
+		# How you would do a multihit:
+		#damages = [damage, damage]
+		#dealt, targets = self.Damage(*damages)
+		#Applies Vulnerable
+		vulnerablePower = powers.Power(timings=powers.TRIGGER.DEFENSE, priority=3, turns=vulnerable, callback=powers.VULNERABLE, AffectDescription=powers.DESCRIPTIONS.VULNERABLE)
+		targets = self.ApplyPowers(vulnerablePower, affectClass=self.Abilities[self.Callbacks.index(self.Scrape)].affectClass,
+						ArenaTargets=1, ArenaSelf=False, ArenaAll=False,
+						GroupTargets=1, GroupOnlySelf=False, GroupIncludeSelf=False, GroupAll=True, GroupCheckAlive=True,
+						extras=[])
+		print(f"{str(self)} uses Scrape to deal RAW_INTENT:{damage} --> ACTUAL:{dealt} damage and apply {vulnerable} vulnerable to {[str(monster) for monster in targets]}!!")
